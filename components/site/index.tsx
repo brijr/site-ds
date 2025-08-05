fc669736e0e1004600efc5eef759cb52647faa7e
Bridger Tower <bridgertower@gmail.com>
2025-08-05 15:27:58 -0600

    add Flex component with customizable layout properties and TypeScript support

--- components/site/index.tsx
+++ components/site/index.tsx
@@ -28,6 +28,13 @@ type GridProps = SharedProps & {
   columns?: 1 | 2 | 3 | 4;
 };
 
+type FlexProps = SharedProps & {
+  direction?: "row" | "column" | "row-reverse" | "column-reverse";
+  wrap?: boolean;
+  justify?: "start" | "end" | "center" | "between" | "around" | "evenly";
+  align?: "start" | "end" | "center" | "baseline" | "stretch";
+};
+
 // Styles
 
 const headingStyles = {
@@ -117,6 +124,58 @@ export const Grid = ({
   return (
     <div
       className={cn("grid gap-4", gridColumns[columns], className)}
+      id={id}
+      style={style}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const Flex = ({
+  children,
+  className,
+  id,
+  style,
+  direction = "row",
+  wrap = false,
+  justify = "start",
+  align = "stretch",
+}: FlexProps) => {
+  const flexDirection = {
+    row: "flex-row",
+    column: "flex-col",
+    "row-reverse": "flex-row-reverse",
+    "column-reverse": "flex-col-reverse",
+  };
+
+  const justifyContent = {
+    start: "justify-start",
+    end: "justify-end",
+    center: "justify-center",
+    between: "justify-between",
+    around: "justify-around",
+    evenly: "justify-evenly",
+  };
+
+  const alignItems = {
+    start: "items-start",
+    end: "items-end",
+    center: "items-center",
+    baseline: "items-baseline",
+    stretch: "items-stretch",
+  };
+
+  return (
+    <div
+      className={cn(
+        "flex gap-4",
+        flexDirection[direction],
+        wrap && "flex-wrap",
+        justifyContent[justify],
+        alignItems[align],
+        className
+      )}
       id={id}
       style={style}
     >
